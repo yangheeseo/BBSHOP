01671fda907268abeadce4156a07b0d58b869468
heeseo <dirntj@gmail.com>
2019-07-24 19:46:42 +0900

    컨트롤러작업

--- src/main/java/com/bbshop/bit/service/BoardServiceImpl.java
+++ src/main/java/com/bbshop/bit/service/BoardServiceImpl.java
@@ -7,40 +7,53 @@ import org.springframework.stereotype.Service;
 import com.bbshop.bit.domain.BoardVO;
 import com.bbshop.bit.mapper.BoardMapper;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
 
 
-
+@Log4j
 @Service
+@AllArgsConstructor
 public class BoardServiceImpl implements BoardService{
 	
 	private BoardMapper mapper;
 	
 	@Override
 	public void register(BoardVO board) {
+		log.info("register......" + board);
+		
+		mapper.insertSelectKey(board);
 		
 	}
 
 	@Override
 	public BoardVO get(Long bno) {
+		
+		log.info("get.........." + bno);
 		// TODO Auto-generated method stub
-		return null;
+		return mapper.read(bno);
 	}
 
 	@Override
 	public boolean modify(BoardVO board) {
+		
+		log.info("modify....." + board);
 		// TODO Auto-generated method stub
-		return false;
+		return mapper.update(board) == 1;
 	}
 
 	@Override
 	public boolean remove(Long bno) {
+		log.info("remove...." + bno);
 		// TODO Auto-generated method stub
-		return false;
+		return mapper.delete(bno) == 1;
 	}
 
 	@Override
 	public List<BoardVO> getList() {
+		
+		log.info("getList..........");
 		// TODO Auto-generated method stub
-		return null;
+		return mapper.getList();
 	}
 }
